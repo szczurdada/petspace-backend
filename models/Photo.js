907e48d7ca27f97a793ac463b77da52d065c66d4
szczurdada <natalyabel99@gmail.com>
2026-04-07 22:07:07 +0200

    feature/86c954zh7-BE-added-photo-deletion

--- models/Photo.js
+++ models/Photo.js
@@ -1,27 +1,24 @@
 const { Schema, model } = require("mongoose");
 
-const Photo = new Schema(
-  {
-    publicId: {
-      type: String,
-      required: true,
-    },
-    caption: {
-      type: String,
-      maxLength: 300,
-      default: null,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
+const Photo = new Schema({
+  publicId: {
+    type: String,
+    required: true,
   },
-  { toJSON: { virtuals: true } }
-);
+  caption: {
+    type: String,
+    maxLength: 300,
+    default: null,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
 
 module.exports = model("Photo", Photo);
